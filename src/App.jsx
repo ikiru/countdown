0ffcf09a1ace61c131a33b980f95b8c42fde1d80
Jeff Winkler <jeff.a.winkler@gmail.com>
2017-11-10 16:13:55 -0600

    added css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
-    return <div>Countdown Champ, App Component</div>;
+    return (
+      <div className="App">
+        <div>Countdown until December 25th, 2017</div>
+        <div>
+          <div>14 Days</div>
+          <div>30 Hours</div>
+          <div>15 Mins</div>
+          <div>30 Secs</div>
+        </div>
+        <div>
+          <input placeholder="new date" />
+          <button>Submit</button>
+        </div>
+      </div>
+    );
   }
 }
 
