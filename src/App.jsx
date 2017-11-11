319edea50f0a0aa00ef843561eeae599a11d1912
Jeff Winkler <jeff.a.winkler@gmail.com>
2017-11-10 18:41:29 -0600

    submit now accepts input

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      deadline: "December 25, 2017"
+      deadline: "December 25, 2017",
+      newDeadline: ""
     };
   }
+
   changeDeadline() {
-    this.setState({ deadline: "November 25, 2017" });
+    this.setState({ deadline: this.state.newDeadline });
   }
 
   render() {
@@ -23,7 +25,11 @@ class App extends Component {
           <div className="Clock-seconds">30 Secs</div>
         </div>
         <div>
-          <input placeholder="new date" />
+          <input
+            placeholder="new date"
+            onChange={event =>
+              this.setState({ newDeadline: event.target.value })}
+          />
           <button onClick={() => this.changeDeadline()}>Submit</button>
         </div>
       </div>
